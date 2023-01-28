0fd981b8151f2d50f668af0739b876b93b3c14a3
juweins <julian.weins@gmx.de>
2023-01-28 23:35:12 +0100

    Added: HashMap struct to dynamically handle date-keys

--- 001_event_stream_processing/src/main.rs
+++ 001_event_stream_processing/src/main.rs
@@ -1,16 +1,25 @@
 // WSL2/Ubuntu users: Make sure that you have pkg-config and libssl-dev installed! 
 
-use serde::Deserialize;
+use serde::{Serialize, Deserialize};
 use reqwest::{Error};
 
-#[derive(Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug)]
 struct Exchange {
     base: String,
     end_date: String,
-    rates: ExchangeRates,
+    rates: Precision,
 }
-#[derive(Deserialize, Debug)]
+// Since the API returns a nested JSON object with the date as the key, we need to flatten the object
+// to be able to deserialize it into a HashMap (and make the struct dynamic)
+#[derive(Serialize, Deserialize, Debug)]
+struct Precision {
+    #[serde(flatten)]
+    rates: std::collections::HashMap<String, ExchangeRates>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
 struct ExchangeRates {
+        CHF: f64,
         GBP: f64,
         USD: f64,
 }
@@ -19,22 +28,24 @@ struct ExchangeRates {
 async fn main() -> Result<(), Error>{
 
     let start_date = "2023-01-01";
-    let end_date = "2023-01-02";
+    let end_date = "2023-01-28";
     let api_key = read_api_key();
 
-    let request_url = format!("https://api.apilayer.com/exchangerates_data/timeseries?start_date={}&end_date={}&base=USD&symbols=EUR,GBP", start_date, end_date);
+    let request_url = format!("https://api.apilayer.com/exchangerates_data/timeseries?start_date={}&end_date={}&base=EUR&symbols=CHF,GBP,USD", start_date, end_date);
     println!("Requesting from: {}", request_url);
  
     let client = reqwest::Client::new();
     let response = client.get(&request_url)
         // REQUEST YOUR OWN API KEY FROM https://apilayer.com/marketplace/exchangerates_data-api
-        .header("apikey", api_key)
+        .header("apikey", api_key.as_str())
         .send()
         .await?
-        .text()
+        .json::<Exchange>()
         .await?;
 
-    println!("Response: {:?}", response); 
+    //let response = std::fs::read_to_string("example_response.json").unwrap();
+    //let parsed_json = serde_json::from_str::<Exchange>(&response).unwrap();
+    println!("Response: {:#?}", response); 
     Ok(())
     
 
