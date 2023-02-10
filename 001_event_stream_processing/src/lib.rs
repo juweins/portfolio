ec88abcc32bc41292595291e420739ce07381072
juweins <julian.weins@gmx.de>
2023-02-10 20:27:55 +0100

    Changed: get_api_key returns option

--- 001_event_stream_processing/src/lib.rs
+++ 001_event_stream_processing/src/lib.rs
@@ -1,16 +1,17 @@
+mod config;
 mod request;
 mod response;
-mod config;
 
+use log::{info, warn};
+use std::collections::HashMap;
 use std::time::Duration;
-use log::info;
 
-use reqwest::{Error};
+use reqwest::Error;
 
 pub mod consumer;
 pub mod producer;
 
-use config::{KafkaConfig, AzureConfig};
+use config::{AzureConfig, KafkaConfig, ApiDetails};
 use response::Exchange;
 
 use azure_storage::prelude::*;
@@ -19,7 +20,7 @@ use azure_storage_blobs::prelude::*;
 // This function calls the API and returns the result formatted as a struct (Exchange, JSON)
 // - The API accepts a start_date and end_date parameter in the format YYYY-MM-DD
 // TODO: Make the symbols dynamic by wrapping in a CLI argument
-pub async fn request_data(start_date: &str, end_date: &str) -> Result<Exchange, Error> {
+pub async fn request_data(api_name: &str, start_date: &str, end_date: &str) -> Result<Exchange, Error> {
     // TODO: URL builder for dynamic base currency and retrieved symbols
     let request_url = format!("https://api.apilayer.com/exchangerates_data/timeseries?start_date={}&end_date={}&base=EUR&symbols=CHF,GBP,USD", start_date, end_date);
     println!("Requesting from: {}", request_url);
@@ -30,7 +31,7 @@ pub async fn request_data(start_date: &str, end_date: &str) -> Result<Exchange, 
         .get(&request_url)
         // REQUEST YOUR OWN (FREE) API KEY FROM https://apilayer.com/marketplace/exchangerates_data-api
         // The API accepts a simple apikey parameter as authentication method
-        .header("apikey", get_api_key())
+        .header("apikey", get_api_key(&api_name).unwrap())
         .send()
         .await?
         // If the request is successful, the response is parsed into a struct
@@ -44,7 +45,6 @@ pub async fn request_data(start_date: &str, end_date: &str) -> Result<Exchange, 
 // - The function uses the azure_sdk_for_rust crate
 // - The function uses the azure_key.json file for necessary details
 pub async fn push_data() -> azure_core::Result<()> {
-    
     // Temporary: Read a saved response from a file
     // This is to avoid hitting the API limit early on
     let blob_body = std::fs::read("example_response.json").unwrap();
@@ -76,26 +76,44 @@ pub async fn push_data() -> azure_core::Result<()> {
 
 // Read the API key from a file
 // TODO: Make this more secure by using a key vault
-fn get_api_key() -> String {
-    String::from_utf8(std::fs::read("secrets/api_key.json").unwrap()).unwrap()
+fn get_api_key(api_name: &str) -> Option<String> {
+    // read the api_key.json and retrieve the api key by name
+    let api_details = serde_json::from_str::<HashMap<String, ApiDetails>>(
+        &std::fs::read_to_string("secrets/api_key.json").unwrap(),
+    )
+    .unwrap();
+
+    // Retrieve ApiDetails matching the api_name
+    let api = api_details.get(api_name);
+
+    if let Some(_) = api {
+            info!("Found API key for {}", api_name);
+            Some(api.unwrap().apikey.to_string())
+        } else {
+            warn!("No API key found for {}", api_name);
+            None
+        }
+
 }
 
 // Read the Azure details from a file
 fn get_azure_details() -> Result<AzureConfig, Error> {
-    // read the azure details from a file and store them in a vector
-    let azure_details =
-        serde_json::from_str::<AzureConfig>(&std::fs::read_to_string("secrets/azure_key.json").unwrap())
-            .unwrap();
+    // Read the azure details from a file and store them in a vector
+    let azure_details = serde_json::from_str::<AzureConfig>(
+        &std::fs::read_to_string("secrets/azure_key.json").unwrap(),
+    )
+    .unwrap();
     Ok(azure_details)
 }
 
 // Read the Kafka details from a file
 // - returns a KafkaConfig struct
 fn get_kafka_details() -> Result<KafkaConfig, Error> {
-    // read the kafka details from a file and store them in a vector
-    let kafka_details =
-        serde_json::from_str::<KafkaConfig>(&std::fs::read_to_string("secrets/kafka_key.json").unwrap())
-            .unwrap();
+    // Read the kafka details from a file and store them in a vector
+    let kafka_details = serde_json::from_str::<KafkaConfig>(
+        &std::fs::read_to_string("secrets/kafka_key.json").unwrap(),
+    )
+    .unwrap();
     Ok(kafka_details)
 }
 
@@ -111,7 +129,7 @@ mod tests {
 
     #[tokio::test]
     async fn test_request_data() {
-        let result = request_data("2023-01-01", "2023-01-28").await;
+        let result = request_data("exchangerates_api", "2023-01-01", "2023-01-28").await;
         assert!(result.is_ok());
     }
 
@@ -139,8 +157,20 @@ mod tests {
 // - The returned value should be a string
 #[test]
 fn test_get_api_key() {
-    let result = get_api_key();
-    assert!(result.is_ascii());
+    let result = get_api_key("exchangerates_api");
+    assert!(result.is_some());
+}
+
+#[test]
+fn test_valid_api_key() {
+    let result = get_api_key("exchangerates_api");
+    assert!(&result.unwrap().is_ascii());
+}
+
+#[test]
+fn test_invalid_api_key() {
+    let result = get_api_key("random_api");
+    assert!(result.is_none());
 }
 
 // Test the get_azure_details function
