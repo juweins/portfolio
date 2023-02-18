884dab32b2696e29109c12fdd6a956cf7e9c17ae
juweins <julian.weins@gmx.de>
2023-02-18 22:37:00 +0100

    Added: Integration test details for kafka

--- 001_event_stream_processing/tests/kafka.rs
+++ 001_event_stream_processing/tests/kafka.rs
@@ -2,8 +2,8 @@
     This file contains the integration tests for the kafka parts
     - The tests are run by calling cargo test
     - The kafka related functions are:
-        - get_kafka_details
-        - push_to_kafka
+    - get_kafka_details
+    - push_to_kafka
 */
 
 #[cfg(test)]
@@ -13,24 +13,40 @@ mod tests {
     use exchange::client::{new_kafka_producer, new_kafka_consumer};
     use exchange::producer::push_to_kafka;
     use exchange::consumer::read_from_kafka;
+
     use rdkafka::error::KafkaError;
     use rdkafka::producer::FutureRecord;
     use rdkafka::util::Timeout;
 
     #[tokio::test]
     async fn test_push_to_kafka() {
-        let result = push_to_kafka("test", "example_response.json").await;
-        assert!(result.is_ok());
+
+        // Initialize the topic and test variables
+        let test_topic = "test";
+        let test_file = "example_response.json";
+        let test_message = std::fs::read(test_file).unwrap();
+        let test_bytes = bytecount::num_chars(&test_message) as u8;
+
+        let result = push_to_kafka(test_topic, test_file).await.expect("Error: Failed to push to Kafka");
+
+
+        let message_count = result.0;
+        let message_size = result.1;
+        let key = result.2;
+
+        assert_eq!(message_count, 1);
+        assert_eq!(message_size, test_bytes);
+        assert_eq!(key.len(), 36);
     }
 
     #[tokio::test]
     async fn test_read_from_kafka() {
 
         // Initialize the topic and test variables
-        let topic = "test";
-        let test = true;
-        let message = "test_message";
-        let message_bytes = bytecount::num_chars(message.as_bytes()) as u8;
+        let test_topic = "test";
+        let test_key = "00000000-0000-0000-0000-000000000000";
+        let test_message = "test_message";
+        let test_message_bytes = bytecount::num_chars(test_message.as_bytes()) as u8;
 
             // Create a new Kafka producer (Consumer is not working here, yet)
             let push = new_kafka_producer().await;
@@ -38,16 +54,16 @@ mod tests {
             // Send consumer in a background thread to avoid blocking
             // Capture the result in a variable for testing
             let consumer = tokio::spawn(async move {
-                let result = read_from_kafka(topic, test).await;
+                let result = read_from_kafka(test_topic).await;
                 result
             });
 
             // Push a message to the topic
             // Capture result to test for successful producer push
             let producer = push.send(
-                FutureRecord::to(topic)
-                    .payload(message)
-                    .key("test"),
+                FutureRecord::to(test_topic)
+                    .payload(test_message)
+                    .key(test_key),
                 Timeout::After(Duration::from_secs(1)),
             ).await;
 
@@ -62,8 +78,7 @@ mod tests {
             // Check if tasks were successful
             assert!(producer.is_ok());
             assert_eq!(message_count, 1);
-            assert_eq!(message_size[0], message_bytes); // Only one message is pushed atm.
-            assert_eq!(total_size, message_bytes as u32); // Should therefore be the same as the message size
+            assert_eq!(message_size[0], test_message_bytes); // Only one message is pushed atm.
+            assert_eq!(total_size, test_message_bytes as u32); // Should therefore be the same as the message size
     }
-
 }
