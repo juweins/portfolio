a4eb6a7b40f88f266458851d4d6c0f6d4e69efa4
juweins <julian.weins@gmx.de>
2023-02-18 09:14:47 +0100

    Added: Background thread to test consumer properly

--- 001_event_stream_processing/tests/kafka.rs
+++ 001_event_stream_processing/tests/kafka.rs
@@ -13,6 +13,7 @@ mod tests {
     use exchange_stream::client::{new_kafka_producer, new_kafka_consumer};
     use exchange_stream::producer::push_to_kafka;
     use exchange_stream::consumer::read_from_kafka;
+    use rdkafka::error::KafkaError;
     use rdkafka::producer::FutureRecord;
     use rdkafka::util::Timeout;
 
@@ -24,19 +25,40 @@ mod tests {
 
     #[tokio::test]
     async fn test_read_from_kafka() {
+
+        // Initialize the topic and test variables
         let topic = "test";
         let test = true;
+        let message = "test_message";
 
+            // Create a new Kafka producer (Consumer is not working here, yet)
             let push = new_kafka_producer().await;
-            let record = FutureRecord::to("test")
-                .payload("test_message")
-                .key("key");
-            let delivery_status = push.send(record, Timeout::After(Duration::from_secs(1)));
+            
+            // Send consumer in a background thread to avoid blocking
+            // Capture the result in a variable for testing
+            let mut consumer = tokio::spawn(async move {
+                let result = read_from_kafka(topic, test).await;
+                result
+            });
 
-            let pull = read_from_kafka("test", test).await;
+            // Push a message to the topic
+            // Capture result to test for successful producer push
+            let producer = push.send(
+                FutureRecord::to(topic)
+                    .payload(message)
+                    .key("test"),
+                Timeout::After(Duration::from_secs(1)),
+            ).await;
 
-            assert!(pull.is_ok());
-            assert_eq!(pull.unwrap(), 0);
+            // Wait for the consumer to finish by timeout (idle mode)
+            let consumer = consumer.await.unwrap();
+
+            // Check if tasks were successful
+            assert!(producer.is_ok());
+            assert!(consumer.is_ok());
+
+            // Check if the consumer received the message
+            assert_eq!(consumer.unwrap(), 1)
     }
 
 }
